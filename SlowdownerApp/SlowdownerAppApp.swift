16c631410280e52c05f5b88eccd6dd6c1a119795
Victor Madelaine <victormadelaine@gmail.com>
2024-02-17 16:27:27 -0500

    Initial Commit

---
+++ SlowdownerApp/SlowdownerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlowdownerAppApp.swift
+//  SlowdownerApp
+//
+//  Created by Victor Madelaine on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SlowdownerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
